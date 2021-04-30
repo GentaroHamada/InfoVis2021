7baefcfd859e90248913163f105b6597fd1dc0f8
hamada <hamada_gentaro@stu.kobe-u.ac.jp>
2021-04-30 18:08:28 +0900

    Add w06_task2.js

---
+++ W06/w06_task2.js
@@ -0,0 +1,111 @@
+d3.csv("https://gentarohamada.github.io/InfoVis2021/W04/data.csv")
+    .then( data => {
+        data.forEach( d => { d.x = +d.x; d.y = +d.y; });
+
+        var config = {
+            parent: '#drawing_region',
+            width: 256,
+            height: 256,
+            margin: {top:10, right:10, bottom:40, left:50}
+        };
+
+        const scatter_plot = new ScatterPlot( config, data );
+        scatter_plot.update();
+    })
+    .catch( error => {
+        console.log( error );
+    });
+
+class ScatterPlot {
+
+    constructor( config, data ) {
+        this.config = {
+            parent: config.parent,
+            width: config.width || 256,
+            height: config.height || 256,
+            margin: config.margin || {top:10, right:10, bottom:10, left:10}
+        }
+        this.data = data;
+        this.init();
+    }
+
+    init() {
+        let self = this;
+
+        self.svg = d3.select( self.config.parent )
+            .attr('width', self.config.width)
+            .attr('height', self.config.height);
+
+        self.chart = self.svg.append('g')
+            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+
+        self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
+        self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
+        //self.bar_position_x = self.inner_height + 20;
+        self.bar_label_x = self.inner_height + 40;
+
+        self.xscale = d3.scaleLinear()
+            .range( [0, self.inner_width] );
+
+        self.yscale = d3.scaleLinear()
+            .range( [0, self.inner_height] );
+
+        self.xaxis = d3.axisBottom( self.xscale )
+            .ticks(4);
+
+        self.yaxis = d3.axisLeft( self.yscale )
+            .ticks(4);
+
+        self.xaxis_group = self.chart.append('g')
+            .attr('transform', `translate(0, ${self.inner_height})`)
+
+        self.yaxis_group = self.chart.append('g')
+            //.attr('transform', `translate(-20, 0)`);
+
+        // X label
+        self.xlabel = self.chart.append('text')
+            .attr('transform', `translate(` + (self.inner_width/2) +`, ${self.bar_label_x})`)
+            .style('text-anchor', 'middle')
+            .text("X-label");
+
+        // Y label
+        self.ylabel = self.chart.append('text')
+            .attr("x", -30)
+            .attr("y", self.inner_height/2)
+            .attr("transform", "rotate(-90, -30, " + self.inner_height/2 + ")")
+            .style("text-anchor", "middle")
+            .text("Y-label");
+        }
+
+    update() {
+        let self = this;
+
+        //const xmin = d3.min( self.data, d => d.x );
+        const xmax = d3.max( self.data, d => d.x );
+        self.xscale.domain( [0, xmax] );
+
+        //const ymin = d3.min( self.data, d => d.y );
+        const ymax = d3.max( self.data, d => d.y );
+        self.yscale.domain( [ymax, 0] );
+
+        self.render();
+    }
+
+    render() {
+        let self = this;
+
+        self.chart.selectAll("circle")
+            .data(self.data)
+            .enter()
+            .append("circle")
+            .attr("cx", d => self.xscale( d.x ) )
+            .attr("cy", d => self.yscale( d.y ) )
+            .attr("r", d => d.r );
+
+        self.xaxis_group
+            .call( self.xaxis );
+        
+        self.yaxis_group
+            .call( self.yaxis );
+    }
+}
